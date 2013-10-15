799e23cb9e0e14da16854ba5722332a84ec7cff0
Andrew Stewart <andrew@stwrt.ca>
2013-10-15 15:11:18 -0700

    Simplify WebMock request stubbing

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,5 +2,12 @@ require 'gabba'
 require 'minitest/autorun'
 require 'minitest/pride'
 
-require 'webmock'
-include WebMock::API
+require 'webmock/minitest'
+WebMock.disable_net_connect!
+
+def stub_analytics(expected_params)
+  stub_request(
+    :get,
+    /www.google-analytics.com\/__utm.gif\?utmac=#{expected_params[:utmac]}&.*/
+  ).to_return(:status => 200, :body => '', :headers => {})
+end
